d8361ca2c235a01ce6b4f310322f6277022cceee
CecileVexe <cecile.valte@gmail.com>
2025-01-15 10:12:50 +0100

    feat: style movieDetail Actor module

--- Mooovies/src/components/MoviesDetails.tsx
+++ Mooovies/src/components/MoviesDetails.tsx
@@ -42,21 +42,52 @@ const MoviesDetails = () => {
   ) : (
     movie && (
       <div className={styles.container}>
-        <h2 className={styles.title}>{movie.title}</h2>
-        <button className={styles.button} onClick={handleAddToWishlist}>
-          Ajouter à la WishList
-        </button>
+        <div className={styles.header}>
+          <h2 className={styles.title}>{movie.title}</h2>
+          <button className={styles.button} onClick={handleAddToWishlist}>
+            Ajouter à la WishList
+          </button>
+        </div>
         <img
-          className={styles.img}
+          className={styles.poster}
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
-        <p className={styles.overview}>{movie.overview}</p>
-        <p>Release Date: {new Date(movie.release_date).toLocaleDateString()}</p>
-        <p>Popularity: {movie.popularity}</p>
-        <p>Vote Average: {movie.vote_average}</p>
-        <p>Vote Count: {movie.vote_count}</p>
-        <p>Genre: {movie.genres.map((genre) => genre.name).join(", ")}</p>
+        <p className={styles.details}>{movie.overview}</p>
+        <p className={styles.details}>
+          Release Date: {new Date(movie.release_date).toLocaleDateString()}
+        </p>
+        <p className={styles.details}>Popularity: {movie.popularity}</p>
+        <p className={styles.details}>Vote Average: {movie.vote_average}</p>
+        <p className={styles.details}>Vote Count: {movie.vote_count}</p>
+        <p className={styles.details}>
+          Genre: {movie.genres.map((genre) => genre.name).join(", ")}
+        </p>
+        <p className={styles.details}>
+          Production Companies:{" "}
+          {movie.production_companies.map((company) => company.name).join(", ")}
+        </p>
+
+        {movieActors?.cast && (
+          <div className={styles.actorSection}>
+            <h3>Actors</h3>
+            <div className={styles.actorList}>
+              {movieActors?.cast.slice(0, 10).map((actor) => (
+                <div key={actor.id} className={styles.actorCard}>
+                  <img
+                    className={styles.actorImage}
+                    src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                    alt={actor.name}
+                  />
+                  <p className={styles.actorName}>{actor.name}</p>
+                  <p className={styles.actorCharacter}>
+                    Character: {actor.character}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     )
   );
